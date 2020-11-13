21fc4b787acfbbd9f20ccbc954d4cf21b00b8ca4
Louys Hong <louyshong1998@gmail.com>
2020-11-13 16:06:44 +0000

    function templates for party.py

---
+++ party.py
@@ -0,0 +1,23 @@
+
+def split_share(n):
+    '''
+    Splits share into n shares
+    '''
+def lagrange_interp(subshares):
+    '''
+    Recombine subshares
+    '''
+def evaluate_mul():
+    '''
+    Evaluates single MUL gate
+    '''
+def evaluate_add():
+    '''
+    Evaluates single ADD gate
+    '''
+def evaluate_circuit():
+    '''
+    Evaluates whole circuit
+    '''
+
+def bgw_protocol(party_no, private_value, network):
