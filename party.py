97f4d43009ee148b1b89ea7c151fcf859438b01d
Louys Hong <louyshong1998@gmail.com>
2020-11-14 15:58:25 +0000

    add comments

--- party.py
+++ party.py
@@ -15,6 +15,7 @@ def split_share(share):
     for deg in range(DEGREE):
         polynomial.append(randint())
 
+    print(polynomial)
     # allocate subshares for all parties
     subshares = {}
     for p in ALL_PARTIES:
@@ -29,6 +30,7 @@ def lagrange_interp(subshares):
     '''
     recomb_vector = {}
 
+    # find recombination vector
     for p, _ in subshares.items():
         numer_list = []
         denom_list = []
@@ -45,6 +47,7 @@ def lagrange_interp(subshares):
 
     terms = []
 
+    # do lagrange interpolation
     for p, subshare in subshares.items():
         terms.append(recomb_vector[p] * subshare)
 
